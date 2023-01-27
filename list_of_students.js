f6532b2fd32879434819a0292251b64f678ab053
Rodrigo de Farias Gomes <shpnft@gmail.com>
2023-01-27 16:53:04 -0400

    adicionando para depois remover...

---
+++ list_of_students.js
@@ -0,0 +1,33 @@
+console.save = function (data, filename) {
+        if (!data) {
+                console.error('Console.save: No data')
+                return;
+        }
+
+        if (!filename) filename = 'story.json'
+
+        if (typeof data === "object") {
+                data = JSON.stringify(data, undefined, 4)
+        }
+
+        var blob = new Blob([data], {
+                type: 'text/json'
+        }),
+                e = document.createEvent('MouseEvents'),
+                a = document.createElement('a')
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+}
+
+var tabela=document.getElementById("formBoletim").children[1];
+for (var i=3; i < tabela.rows.length - 1; i++) {
+    chuchu[i-3]=tabela.rows[i].cells[2].innerHTML;
+}
+
+console.save(chuchu, "ehoje.json")
+
+
---
+++ tests/bora.js
@@ -0,0 +1,210 @@
+document.getElementById("dtAula").value="18/10/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Apresentação do plano de ensino; definição de vetores; uso de vetores na Física e Matemática'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="20/10/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Operações com vetores: soma e multiplicação por escalar'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="25/10/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Participação na Semana Nacional de Ciência e Tecnologia'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="27/10/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Participação na Semana Nacional de Ciência e Tecnologia'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="03/11/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="08/11/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Dependência linear, bases e coordenadas de um vetor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="10/11/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="17/11/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="22/11/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Produto interno; ângulo entre vetores; projeção de um vetor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="24/11/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="29/11/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='prova'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="01/12/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Produto vetorial e produto misto'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="06/12/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Sistemas de coordenadas'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="13/12/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="15/12/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Equação da reta e do plano'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="20/12/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="22/12/2022"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Distância entre ponto e reta; Distância entre ponto e plano'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="03/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="05/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Distância entre duas retas; distância entre dois planos'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="10/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="12/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='prova'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="17/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Definição (e dedução da equação) de elipse'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="19/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="24/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Definição (e dedução da equação) de parábola'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="26/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="31/01/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Definição (e dedução da equação) de hipérbole'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="02/02/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="07/02/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Estudo de propriedades de cônicas – equações típicas'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="09/02/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='Resolução de exercícios em sala com orientação do professor'
+document.getElementById("adicionarCronograma").click()
+
+document.getElementById("dtAula").value="14/02/2023"
+document.getElementById("cargaHoraria").value=2
+document.getElementById("tipoAula").value=document.getElementById("tipoAula1").options[0].value
+document.getElementById("contratoRh").value=document.getElementById("contratoRh1").options[0].value
+document.getElementById("conteudo").value='prova'
+document.getElementById("adicionarCronograma").click()
+
