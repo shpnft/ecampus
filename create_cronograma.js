f3916618ed0d69cbb47853a485ce0a9456303394
Rodrigo de Farias Gomes <shpnft@gmail.com>
2023-02-08 22:33:11 -0400

    nada demais

--- create_cronograma.js
+++ create_cronograma.js
@@ -1,28 +1,22 @@
-var input = document.createElement('input');
-input.type = 'file';
-
-input.onchange = e => { 
-
-        // getting a hold of the file reference
-        var file = e.target.files[0]; 
-
-        // setting up the reader
-        var reader = new FileReader();
-        reader.readAsText(file,'UTF-8');
-
-        // here we tell the reader what to do when it's done reading...
-        reader.onload = readerEvent => {
-                var content = JSON.parse(readerEvent.target.result);
-                for (var i=0; i<content.length; i++) {
-                        document.querySelector("#dtAula").value=content[i][0];
-                        document.querySelector("#cargaHoraria").value=content[i][1];
-                        document.querySelector("#tipoAula").value=document.querySelector("#tipoAula").options[content[i][2]].value;
-                        document.querySelector("#contratoRh").value=document.querySelector("#contratoRh").options[content[i][3]].value;
-                        document.querySelector("#conteudo").value=content[i][4];
-                        document.querySelector("#adicionarCronograma").click();
+javascript: (() => {
+        var input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = e => { 
+                var file = e.target.files[0]; 
+                var reader = new FileReader();
+                reader.readAsText(file,'UTF-8');
+                reader.onload = readerEvent => {
+                        var content = JSON.parse(readerEvent.target.result);
+                        for (var i=0; i<content.length; i++) {
+                                document.querySelector("#dtAula").value=content[i][0];
+                                document.querySelector("#cargaHoraria").value=content[i][1];
+                                document.querySelector("#tipoAula").value=document.querySelector("#tipoAula").options[content[i][2]].value;
+                                document.querySelector("#contratoRh").value=document.querySelector("#contratoRh").options[content[i][3]].value;
+                                document.querySelector("#conteudo").value=content[i][4];
+                                document.querySelector("#adicionarCronograma").click();
+                        }
                 }
-        }
-
+        };
+        input.click();
 }
-
-input.click();
+)();
--- delete_cronograma.js
+++ delete_cronograma.js
@@ -1,7 +1,9 @@
-var n=document.getElementById("updateMeCronograma").children[0].children[0].rows.length;
-var bob = new Array(n-1);
-for (var i=0; i < n-1; i++) {
-        bob[i]=document.getElementById("updateMeCronograma").children[0].children[0].rows[i+1].cells[6].children[0].name;
+javascript: (() => {
+        var n=document.getElementById("updateMeCronograma").children[0].children[0].rows.length;
+        var bob = new Array(n-1);
+        for (var i=0; i < n-1; i++) {
+                bob[i]=document.getElementById("updateMeCronograma").children[0].children[0].rows[i+1].cells[6].children[0].name;
+        };
+        bob.forEach(excluirCronograma)
 }
-
-bob.forEach(excluirCronograma)
+) ();
