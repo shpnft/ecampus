0796726e2777abfb8081fdce2db1be6cb15e8878
Rodrigo de Farias Gomes <shpnft@gmail.com>
2023-02-13 18:01:09 -0400

    nada demais

--- delete_cronograma.js
+++ delete_cronograma.js
@@ -1,9 +1,5 @@
 javascript: (() => {
-        var n=document.getElementById("updateMeCronograma").children[0].children[0].rows.length;
-        var bob = new Array(n-1);
-        for (var i=0; i < n-1; i++) {
-                bob[i]=document.getElementById("updateMeCronograma").children[0].children[0].rows[i+1].cells[6].children[0].name;
-        };
-        bob.forEach(excluirCronograma)
+        const bob=document.querySelectorAll("#excluirCronograma");
+        bob.forEach((b) => {excluirCronograma(b.name)});
 }
 ) ();
