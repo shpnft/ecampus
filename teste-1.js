8eb7bb7938e7ab3f8f8782850116a10c79373f80
Rodrigo de Farias Gomes <shpnft@gmail.com>
2023-02-17 08:54:42 -0400

    Nada demais

--- teste-1.js
+++ teste-1.js
@@ -1,3 +1,4 @@
+const bob=document.querySelectorAll(".tbodyBoletim");
 var input = document.createElement('input');
 input.type = 'file';
 input.onchange = e => { 
@@ -8,6 +9,14 @@ input.onchange = e => {
                 var ttt={};
                 var content = JSON.parse(readerEvent.target.result);
                 content.forEach(x => ttt[x[0]]=x.slice(1));
+                for (var i=0; i < bob.length; i++) {
+                        const asd=Array.from(bob[i].cells).map(td => td.firstChild);
+                        var mat=asd[1].textContent;
+                        if (ttt[mat]) {
+                                var j=0;
+                                asd.slice(3,13).filter(e => !e.disabled).forEach(e => e.value=ttt[mat][j++] ?? 0);
+                        }
+                }
         }
 }
 input.click();
