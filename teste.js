69a30ee36163f2653ddc530f4caefe13886283ea
Rodrigo de Farias Gomes <shpnft@gmail.com>
2023-02-16 22:02:18 -0400

    Mais do mesmo

--- teste.js
+++ teste.js
@@ -1,4 +1,14 @@
-bob=document.querySelectorAll("#formBoletim > table > tbody > tr.tbodyBoletim");
-bob[0].cells[1].innerText
-
-
+javascript: (() => {
+const bob=document.querySelectorAll(".tbodyBoletim");
+var alunos = [];
+for (var i=0; i < bob.length; i++) {
+        let aluno = [];
+        const asd=Array.from(bob[i].cells).map(td => td.firstChild);
+        asd.slice(1,3).forEach(e => aluno.push(e.textContent));
+        asd.slice(3,13).filter(e => !e.disabled).forEach(e => aluno.push(e.value));
+        alunos.push(aluno);
+}
+previewWinRef = window.open("about:blank");
+previewWinRef.document.write(alunos.join("<br>"));
+}
+)();
