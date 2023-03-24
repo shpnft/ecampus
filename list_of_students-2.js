e4610417e79c72aaa71a9f3bbdfbb274d445df24
Rodrigo de Farias Gomes <shpnft@gmail.com>
2023-03-24 07:08:39 -0400

    nada demais

--- list_of_students-2.js
+++ list_of_students-2.js
@@ -4,7 +4,7 @@ var alunos = [];
 for (var i=0; i < bob.length; i++) {
         let aluno = [];
         const asd=Array.from(bob[i].cells).map(td => td.firstChild);
-        asd.slice(1,3).forEach(e => aluno.push(e.textContent));
+        asd.slice(2,3).forEach(e => aluno.push(e.textContent));
         asd.slice(3,13).filter(e => !e.disabled).forEach(e => aluno.push(e.value));
         alunos.push(aluno);
 }
