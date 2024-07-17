d61c0c80a75b228a3e827f9ed08a99535da184ab
Rodrigo de Farias Gomes <shpnft@gmail.com>
2024-07-17 11:15:16 -0400

    refatorando

--- csv2json.py
+++ csv2json.py
@@ -1,5 +1,7 @@
 import json,csv
 import argparse
+import re
+from datetime import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("csv_filename")
@@ -15,8 +17,25 @@ with open(args.json_filename,"w",encoding="utf-8") as f:
 
 # checking
 dados.pop(0) # removendo o cabeçalho
+
+# texto colorido
+WARNING = "\033[93m"
+ENDC = "\033[0m"
+aviso=lambda x: print(f"{WARNING}"+x+f"{ENDC}")
+
 soma=0
 for l in dados:
     soma=soma+int(l[1])
+
+    if re.match(r"\d\d/\d\d/\d\d\d\d",l[0]) is None:
+        aviso(f"{l[0]} é uma data inválida (não está no formato DD/MM/YYYY)")
+    try:
+        data = datetime.strptime(l[0],"%d/%m/%Y")
+    except ValueError:
+        aviso(f"{l[0]} é uma data inválida (a função strptime não reconheceu)")
+
+    if not len(l) == 5:
+        aviso("Todas as linhas devem ter 5 colunas: data, carga horária, tipo de aula, conteúdo, professor")
+
 print(f"{soma} horas de aula distribuídas em {len(dados)} aulas")
 
