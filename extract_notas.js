75bd3c0bb63c6c850e5b9647bb15f3df94817a86
Rodrigo de Farias Gomes <shpnft@gmail.com>
2024-07-31 10:05:16 -0400

    extract_notas

--- extract_notas.js
+++ extract_notas.js
@@ -1,5 +1,19 @@
-a=document.querySelectorAll("input.exercicio:not([disabled])")
-c=[]
-for (i=0; i < a.length;i++) c.push(a[i].value)
-d=window.open("about:blank")
-d.document.write(c.join("<br>")
+javascript: (function () {
+        const tabela=document.querySelectorAll("tr.tbodyBoletim:not(.trancado)");
+        var notas=[];
+        for (i=0; i < tabela.length; i++) {
+                temp=[];
+                /* posição na lista, matrícula, nome */
+                for (j=0; j < 3; j++) temp.push(tabela[i].cells[j].innerText);
+
+                /* const linha=tabela[i].querySelectorAll("input.nota:not([disabled]):not(.prova)");*/
+                const linha=tabela[i].querySelectorAll("input.nota:not([disabled])");
+                for (j=0; j < linha.length; j++) temp.push(linha[j].value);
+
+                /*notas.push(temp.toString());*/
+                notas.push("\""+temp.join("\",\"")+"\"");
+        }
+        var saida=window.open("about:blank");
+        saida.document.write(notas.join("<br>"));
+}
+)();
