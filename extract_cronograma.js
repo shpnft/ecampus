dd960a5240e76259134c6306d80a5740502fa74e
Rodrigo de Farias Gomes <shpnft@gmail.com>
2026-05-19 14:42:02 -0400

    enxugando o código

--- extract_cronograma.js
+++ extract_cronograma.js
@@ -1,24 +1,36 @@
-javascript: (function () {
-        var table = document.querySelector("#updateMeCronograma > div.list > table");
+javascript:(function () {
+    var table = document.querySelector("#updateMeCronograma > div.list > table");
+    if (!table) {
+        alert("Tabela não encontrada.");
+        return;
+    }
 
-        /*  https://stackoverflow.com/a/60196347/20287521 */
-        var data = [];
-        for (var i = 1; i < table.rows.length; i++) {
-                var tableRow = table.rows[i];
-                var rowData = [];
-                for (var j = 0; j < tableRow.cells.length; j++) {
-                        rowData.push(tableRow.cells[j].innerHTML);
-                }
-                data.push(rowData);
+    function csvEscape(value) {
+        value = (value ?? "").toString();
+        return '"' + value.replace(/"/g, '""') + '"';
+    }
+
+    var rows = [];
+
+    for (var i = 1; i < table.rows.length; i++) {
+        var tableRow = table.rows[i];
+        var rowData = [];
+
+        for (var j = 0; j < tableRow.cells.length; j++) {
+            rowData.push(csvEscape(tableRow.cells[j].textContent));
         }
 
-        /* https://stackoverflow.com/a/30800715/20287521 */
-        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(data));
-        var downloadAnchorNode = document.createElement('a');
-        downloadAnchorNode.download = "cronograma.json";
-        downloadAnchorNode.href = dataStr;
-        document.body.appendChild(downloadAnchorNode); /* required for firefox */
-        downloadAnchorNode.click();
-        downloadAnchorNode.remove();
-}
-)();
+        rows.push(rowData.join(";"));
+    }
+
+    /* BOM ajuda o Excel a reconhecer UTF-8 corretamente */
+    var csvContent = "\uFEFF" + rows.join("\r\n");
+    var dataStr = "data:text/csv;charset=utf-8," + encodeURIComponent(csvContent);
+
+    var downloadAnchorNode = document.createElement("a");
+    downloadAnchorNode.download = "cronograma.csv";
+    downloadAnchorNode.href = dataStr;
+    document.body.appendChild(downloadAnchorNode);
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+})();
